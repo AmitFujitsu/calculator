f63fa05fee32920c7ed2f5fa1a52b044b572bb67
AmitFujitsu <amit.k@fujitsu.com>
2023-01-11 15:36:48 +0530

    Updates

---
+++ precache-manifest.6603f24f705f0f519138c7d55fb9f248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be8e3c36cc14277005708560ab41554",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "1fc2cf7e70b295f59ce0",
+    "url": "/calculator/static/css/main.37131298.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "1fc2cf7e70b295f59ce0",
+    "url": "/calculator/static/js/main.518fae14.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
